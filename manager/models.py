d5905d016a5bb5e5023b7f9872a328dd7c252bb9
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-02 14:44:36 +0100

    feat(models.py): Created Task model, installed django-taggit for tags, added taggit to INSTALLED_APPS

--- manager/models.py
+++ manager/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
+from task_manager import settings
 
 
 class Worker(AbstractUser):
@@ -34,5 +37,69 @@ class TaskType(models.Model):
     def get_absolute_url(self):
         return reverse(
             "manager:task-type-detail",
+            kwargs={"pk": self.pk}
+        )
+
+
+class Task(models.Model):
+    class StatusChoices(models.TextChoices):
+        TO_DO = "TD", "To Do"
+        IN_PROGRESS = "IP", "In Progress"
+        DONE = "DN", "Done"
+        ARCHIVED = "AR", "Archived"
+
+    class PriorityOptions(models.TextChoices):
+        URGENT = "UR", "Urgent"
+        HIGH_PRIORITY = "HP", "High Priority"
+        MEDIUM_PRIORITY = "MP", "Medium Priority"
+        LOW_PRIORITY = "LP", "Low Priority"
+        ROUTINE = "RT", "Routine"
+        OPTIONAL = "OP", "Optional"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+
+    priority = models.CharField(
+        max_length=2,
+        choices=PriorityOptions.choices,
+        default=PriorityOptions.MEDIUM_PRIORITY
+    )
+
+    status = models.CharField(
+        max_length=2,
+        choices=StatusChoices.choices,
+        default=StatusChoices.TO_DO,
+    )
+
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        null=True,
+        blank=True
+    )
+
+    task_tag = TaggableManager(blank=True)
+
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="assigned_tasks"
+    )
+
+    @property
+    def is_completed(self):
+        return self.status == self.StatusChoices.DONE
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.name} ({self.priority})"
+
+    def get_absolute_url(self):
+        return reverse(
+            "manager:task-detail",
             kwargs={"pk": self.pk}
         )
