5c9075163bf155d94ee6eb8ef9fac3709778221a
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-02 19:08:56 +0100

    feat(admin.py): Registered models and customized admin panel

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from manager.models import Worker, Task, TaskType, Commentary
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}
+         ),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info",
+         {"fields": ("first_name", "last_name", "position",)}
+         ),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ["name", "task_type", "priority"]
+    list_display = ["name", "task_type", "priority"]
+
+
+admin.site.register(TaskType)
+admin.site.register(Commentary)
+admin.site.unregister(Group)
