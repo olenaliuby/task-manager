fd75c1c79a3ccccdde3775945f79887588e4d1fc
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-03 13:37:39 +0100

    feat: Configurated authentication, added login/logout pages with includes & base-fullscreen.html

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("manager.urls", namespace="manager")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
