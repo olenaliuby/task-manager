9a8e679183c4afcec6341df3f2b8ab7a317bce5c
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-03 15:21:56 +0100

    feat(urls): Added urls for Worker CRUD

--- manager/urls.py
+++ manager/urls.py
@@ -3,10 +3,30 @@ URL configuration for manager app.
 """
 from django.urls import path
 
-from manager.views import index
+from manager.views import (
+    index,
+    WorkerListView,
+    WorkerCreateView,
+    WorkerDetailView,
+    WorkerUpdateView,
+    WorkerDeleteView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("team/", WorkerListView.as_view(), name="worker-list"),
+    path("team/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("team/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path(
+        "team/<int:pk>/update/",
+        WorkerUpdateView.as_view(),
+        name="worker-update"
+    ),
+    path(
+        "team/<int:pk>/delete/",
+        WorkerDeleteView.as_view(),
+        name="worker-delete"
+    ),
 ]
 
 app_name = "manager"
