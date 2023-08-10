4f0cf4ea70573cbd1eacbd80ab1bea5b24aeef0b
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-10 13:16:38 +0100

    refactor: Removed insecure Django secret key from settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    default="django-insecure-br1#1l+48r41+rrt5lvz+*zam4@#x+!nv@fro60^#s55)a$nrn"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
